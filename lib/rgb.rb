b40af15e7e40afba095eec60781a7c9ffbf53232
Yuki Harada <y.harada59@gmail.com>
2020-08-21 11:58:03 +0900

    Improve rgb on DRY

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,8 +1,14 @@
 def to_hex(r,g,b)
-    '#000000'
-    '#' +
-        r.to_s(16).rjust(2,'0') +
-        g.to_s(16).rjust(2,'0') +
-        b.to_s(16).rjust(2,'0')
-        
+    hex = '#'    
+    [r,g,b].each do |n|
+        hex += n.to_s(16).rjust(2,'0')
+    end
+
+    hex
+
+    #before refactoring
+    # r.to_s(16).rjust(2,'0') +
+    # g.to_s(16).rjust(2,'0') +
+    # b.to_s(16).rjust(2,'0')
+
 end
