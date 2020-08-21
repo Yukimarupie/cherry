a70183135fae06e9cc5148d286c750ed05e44c6b
Yuki Harada <y.harada59@gmail.com>
2020-08-21 12:01:01 +0900

    Improve rgb using  inject method

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,12 +1,19 @@
 def to_hex(r,g,b)
-    hex = '#'    
-    [r,g,b].each do |n|
-        hex += n.to_s(16).rjust(2,'0')
+    [r,g,b].inject('#') do |hex, n|
+        hex + n.to_s(16).rjust(2,'0')
     end
+    
+    
+    
+    #[2] using each method
+    # hex = '#'    
+    # [r,g,b].each do |n|
+    #     hex += n.to_s(16).rjust(2,'0')
+    # end
 
-    hex
+    # hex
 
-    #before refactoring
+    #[1] before refactoring
     # r.to_s(16).rjust(2,'0') +
     # g.to_s(16).rjust(2,'0') +
     # b.to_s(16).rjust(2,'0')
