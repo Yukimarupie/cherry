b6f9d009e7b459006bbda4374ff2352550634632
Yuki Harada <y.harada59@gmail.com>
2020-09-02 13:20:28 +0900

    Add enter and stamp method

--- lib/gate/gate.rb
+++ lib/gate/gate.rb
@@ -3,8 +3,10 @@ class Gate
         @name = name #受け取った駅の名前はあとで使えるようにインスタンス変数へ格納
     end
 
-    def enter(ticket)
-
+    #enterメソッドは、引数として渡された切符(Ticket)に自分の駅名を保存する
+    #Ticketクラスのstampメソッドを参照(stampメソッドに駅名を渡すとその駅名がTicketクラスのインスタンスに保持される)
+    def enter(ticket) 
+        ticket.stamp(@name)
     end
 
     def exit(ticket)
--- lib/gate/ticket.rb
+++ lib/gate/ticket.rb
@@ -1,5 +1,12 @@
 class Ticket
+    #運賃と乗車駅が外部から取得できるようにゲッターメソッドを設定
+    attr_reader :fare, :stamped_at 
+
     def initialize(fare)
          @fare = fare
+    end
+
+    def stamp(name)
+        @stamped_at = name #乗車駅(stamp_at)
     end
 end
