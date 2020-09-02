4bae4f18fef9a749abc74a77283f1109724d9ce7
Yuki Harada <y.harada59@gmail.com>
2020-09-02 13:34:34 +0900

    Add calc_fare method

--- lib/gate/gate.rb
+++ lib/gate/gate.rb
@@ -1,4 +1,10 @@
 class Gate
+
+    #運賃と駅名を配列として用意
+    STATIONS = [:umeda, :juso, :mikuni]
+    FARES = [150, 190]
+
+
     def initialize(name) #引数として駅の名前を受け取れるようにする
         @name = name #受け取った駅の名前はあとで使えるようにインスタンス変数へ格納
     end
@@ -11,5 +17,18 @@ class Gate
 
     def exit(ticket)
         true
+    end
+
+    def calc_fare(ticket)
+        from = STATIONS.index(ticket.stamped_at)
+        to = STATIONS.index(@name)
+        distance = to - from
+        FARES[distance - 1]
+
+        #ここのindexメソッドは配列の中から引数に合致する要素の添字を取得するメソッド
+        #[:umeda, :juso, :mikuni].index(:juso) => 1
+        #例)
+        #乗車駅が「:umeda」、降車駅が「:mikuni」の場合、変数from = 0、変数to = 2、distanceは2-0=2
+        #配列FARESから値を取得する場合はdistanceの値から1を引く。FARES[1] = 190
     end
 end
