2b081d2446ff949e4ad89388baa4bdf8bdb452b3
Yuki Harada <y.harada59@gmail.com>
2020-09-05 00:48:39 +0900

    9.5.5 Add exception handling

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -2,11 +2,16 @@ print 'Text?:'
 #printメソッドは文字列を表示した後に改行しない
 text = gets.chomp
 
-print 'Pattern?:'
-pattern = gets.chomp
+#例外処理を組み込んで、再入力を可能にする。
+begin
+    print 'Pattern?:'
+    pattern = gets.chomp
+    regexp = Regexp.new(pattern)
+rescue RegexpError => e
+    puts "Invalid pattern: #{e.message}"
+    retry
+end
 
-
-regexp = Regexp.new(pattern)
 matches = text.scan(regexp)
 if matches.size > 0
     puts "Matched: #{matches.join(',')}"
