ac14d28c3e2125fe442131d322e9d31f425be32d
Yuki Harada <y.harada59@gmail.com>
2020-09-04 11:05:12 +0900

    Def deep_freeze module

--- lib/deep_freezable/deep_freezable.rb
+++ lib/deep_freezable/deep_freezable.rb
@@ -1,5 +1,10 @@
 module DeepFreezable
-    def deep_freeze(arrauy_or_hash)
-        
+    def deep_freeze(array_or_hash)
+        #配列の各要素をfreeze
+        array_or_hash.each do |element|
+            element.freeze
+        end
+        #配列自身をfreeze & これがメソッドの戻り値に。
+        array_or_hash.freeze
     end
 end
--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -3,8 +3,12 @@ require './lib/deep_freezable/bank'
 require './lib/deep_freezable/team'
 
 class DeepFreezableTest < Minitest::Test
-    def test_deep_freeze
-        assert DeepFreezable
+    def test_deep_freeze_to_array
+        #配列の値が正しいか
+        assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
+        #配列自身がfreezeされてるか
+        assert Team::COUNTRIES.frozen?
+        #配列の要素が全てfreezeされているか
+        assert Team::COUNTRIES.all? {|country| country.frozen? }
     end
 end
-
