e249b1059806105a0fba5b62c832b79c3312d5b1
Yuki Harada <y.harada59@gmail.com>
2020-09-04 11:22:54 +0900

    Add deep_freeze method for hash

--- lib/deep_freezable/deep_freezable.rb
+++ lib/deep_freezable/deep_freezable.rb
@@ -1,10 +1,18 @@
 module DeepFreezable
     def deep_freeze(array_or_hash)
+        case array_or_hash
+        when Array
         #配列の各要素をfreeze
-        array_or_hash.each do |element|
-            element.freeze
+            array_or_hash.each do |element|
+                element.freeze
+            end
+        when Hash 
+            array_or_hash.each do |key, value|
+                key.freeze
+                value.freeze
+            end            
         end
-        #配列自身をfreeze & これがメソッドの戻り値に。
+        #配列とハッシュ自身をfreeze & これがメソッドの戻り値に。
         array_or_hash.freeze
     end
 end
