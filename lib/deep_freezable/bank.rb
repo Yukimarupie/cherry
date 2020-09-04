3331809129471c0c2ba1e26ba03683103a9b5dbb
Yuki Harada <y.harada59@gmail.com>
2020-09-04 11:14:57 +0900

    Add test for bank.rb

--- lib/deep_freezable/bank.rb
+++ lib/deep_freezable/bank.rb
@@ -3,6 +3,6 @@ require './lib/deep_freezable/deep_freezable'
 class Bank
     extend DeepFreezable
 
-    COUNTRIES = deep_freeze({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
+    CURRENCIES = deep_freeze({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
 
 end
